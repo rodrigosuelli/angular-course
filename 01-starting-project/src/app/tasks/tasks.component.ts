8ae67f78e43ac2d811d4c66167647762c92fcd28
rodrigosuelli <rodrigosuelli@gmail.com>
2025-12-22 22:25:27 -0300

    Sec 2 class 62

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -21,8 +21,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {}
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
